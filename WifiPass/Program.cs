ffc7c2ac46bbe3008b2522556f0d11a0182c2ccc
jon <57006360+tremlin84@users.noreply.github.com>
2020-01-18 01:52:23 +0000

    Add files via upload

---
+++ WifiPass/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace WifiPass
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Title = "";
+            FindAP("netsh wlan show profile");            
+            Console.Read();
+        }
+        
+        private static void FindAP(string input)
+        {
+            try
+            {                
+                ProcessStartInfo procStartInfo = new ProcessStartInfo("cmd", "/c " + input)
+                { 
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,                    
+                    CreateNoWindow = true
+                };
+                                
+                Process proc = new Process
+                {
+                    StartInfo = procStartInfo
+                };
+                proc.Start();
+                                
+                AccessPointGetter(proc.StandardOutput.ReadToEnd());
+                proc.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
+        }
+
+        private static void AccessPointGetter(string input)
+        {
+            var ListOfWords = input.Split();
+
+            for (int i = 0; i < ListOfWords.Length; i++)
+            {
+                if (ListOfWords[i] == ":")
+                {
+                    i++;
+                    AccessPoints(ListOfWords[i]);
+                }
+            }
+        }
+
+        private static void AccessPoints(string input)
+        {
+            string command = "netsh wlan show profile " + input + " key=clear";
+            try
+            {
+                ProcessStartInfo procStartInfo = new ProcessStartInfo("cmd", "/c " + command)
+                {
+                    RedirectStandardOutput = true,                    
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                Process proc = new Process
+                {
+                    StartInfo = procStartInfo                    
+                };
+                proc.Start();
+
+                string a = proc.StandardOutput.ReadToEnd();
+                
+                string lastWord = a.Split(' ').Last();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Access Point: {input} Password: {lastWord}");
+
+                proc.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }    
+}

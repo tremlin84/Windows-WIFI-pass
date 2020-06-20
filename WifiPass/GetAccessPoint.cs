012cd85fb945ea46afc3f1f6d42012dedf13688a
jon <57006360+tremlin84@users.noreply.github.com>
2020-06-20 03:42:20 +0100

    Updated now supports windows 10
    Tested windows 7 + 10 should work with all windows.

---
+++ WifiPass/GetAccessPoint.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace WifiPass
+{
+    class GetAccessPoint
+    {
+        public List<string> AccessPoint = new List<string>();
+        public GetAccessPoint(string command = "netsh wlan show profile")
+        {
+            if (command.EndsWith("profile"))
+            {
+
+            }
+            try
+            {
+                ProcessStartInfo procStartInfo = new ProcessStartInfo("cmd", "/c " + command)
+                {
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                Process proc = new Process
+                {
+                    StartInfo = procStartInfo
+                };
+                proc.Start();
+
+                string standard_output;
+                while ((standard_output = proc.StandardOutput.ReadLine()) != null)
+                {
+                    if (standard_output.Contains("User Profile"))
+                    {
+                        string[] ap = standard_output.Split(':');
+                        AccessPoint.Add(ap[1]);                        
+                    }
+                }
+                proc.Dispose();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error finding access points");
+
+            }            
+        }
+    }
+}
---
+++ WifiPass/GetApPassword.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace WifiPass
+{
+    class GetApPassword
+    {
+        public GetApPassword(string input)
+        {
+            string command = "netsh wlan show profile" + input + " key=clear";
+            try
+            {
+                ProcessStartInfo procStartInfo = new ProcessStartInfo("cmd", "/c " + command)
+                {
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                Process proc = new Process
+                {
+                    StartInfo = procStartInfo
+                };
+                proc.Start();
+
+                string results;
+                while ((results = proc.StandardOutput.ReadLine()) != null)
+                {
+                    if (results.Contains("Key Content"))
+                    {
+                        string[] ap = results.Split(':');                        
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"Access Point:{input} Password:{ap[1]}");
+                    }                    
+                }                
+
+                proc.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
